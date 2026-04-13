930368a7d41e5f90bf0f2d83f581d8efa872ddc5
liorbel1 <liorbel534@gmail.com>
2026-04-13 17:41:11 +0300

    added date and weekly pictures

--- YuvalRubinWebsite/MasterPage.master.cs
+++ YuvalRubinWebsite/MasterPage.master.cs
@@ -9,6 +9,45 @@ public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            // Display the current server date in the header
+            DateLabel.Text = DateTime.Now.ToString("d");
+
+            // Set the image based on the day of the week
+            string dayOfWeek = DateTime.Now.DayOfWeek.ToString();
+            string imagePath = GetImagePathForDay(dayOfWeek);
+
+            DayImage.ImageUrl = imagePath;
+            DayImage.AlternateText = "Image for dayOfWeek" + dayOfWeek;
+        }
+
 
     }
+    private string GetImagePathForDay(string dayOfWeek)
+    {
+        string path = "images/week/";
+        // Use different image paths for each day
+        switch (dayOfWeek)
+        {
+            case "Sunday":
+                return path + "sunday.png";
+            case "Monday":
+                return path + "monday.png";
+            case "Tuesday":
+                return path + "tuesday.png";
+            case "Wednesday":
+                return path + "wednesday.png";
+            case "Thursday":
+                return path + "thursday.png";
+            case "Friday":
+                return path + "friday.png";
+            case "Saturday":
+                return path + "saturday.png";
+            default:
+                return path + "default.png"; // Fallback image
+        }
+    }
+
 }
+
